f26e6e9d572aed5c16d49f94ce14dfe3667e02fc
Rahul Vaidya <rahulnkvaidya@yahoo.co.in>
2026-02-15 09:51:59 +0530

    Update navbar and footer links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -87,7 +87,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Documentation',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
@@ -104,8 +104,8 @@ const config: Config = {
           title: 'Docs',
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: 'Documentation',
+              to: '/docs/',
             },
           ],
         },
